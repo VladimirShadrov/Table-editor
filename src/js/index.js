fc3ab73772f2fa297b916abfa082772518fd8956
Vladimir <shadrov_vladimir@mail.ru>
2021-04-19 18:40:53 +0300

    Добавлена динамическая отрисовка модальных окон и функционал открытия/закрытия модальных окон

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import '../styles/styles.scss';
-import '../styles/editor.scss';
-import '../styles/modal.scss';
-import { drowCurrentProductsElements } from './helpers/helpers';
-
-const editor = document.querySelector('.editor');
-const productsBox = document.querySelector('.box');
-
-drowCurrentProductsElements('currentItems', productsBox);
