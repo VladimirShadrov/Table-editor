3163f8ca0cb10351c2a11adb50a4b902d44304db
Vladimir <shadrov_vladimir@mail.ru>
2021-04-19 17:19:01 +0300

    Добавлены вспомогательные функции, создан и добавлен в localeStorage массив с данными

---
+++ src/js/data/data.js
@@ -0,0 +1,38 @@
+import { setToLocalStorage } from '../helpers/helpers';
+
+export const defaultData = [
+  {
+    name: 'square',
+    type: 'square',
+    color: 'rgba(84, 135, 114, 1)',
+    id: 1,
+    class: 'box__image-square',
+    viewingClass: 'modal-show__square',
+  },
+  {
+    name: 'circle',
+    type: 'circle',
+    color: 'rgba(141, 41, 143, 1)',
+    id: 2,
+    class: 'box__image-circle',
+    viewingClass: 'modal-show__circle',
+  },
+  {
+    name: 'rectangle',
+    type: 'rectangle',
+    color: 'rgba(157, 188, 235, 1)',
+    id: 3,
+    class: 'box__image-rectangle',
+    viewingClass: 'modal-show__rectangle',
+  },
+  {
+    name: 'oval',
+    type: 'oval',
+    color: 'rgba(60, 50, 120, 1)',
+    id: 4,
+    class: 'box__image-oval',
+    viewingClass: 'modal-show__oval',
+  },
+];
+
+setToLocalStorage('currentItems', defaultData);
